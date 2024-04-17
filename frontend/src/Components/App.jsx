30aa498c479bf346486d9650f6e8396b143117c5
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-04-17 13:50:35 -0500

    WIP adding UserProfile

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./SignIn";
 import Register from "./Register";
+import UserProfile from "./UserProfile";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/userprofile" element={<UserProfile />} />
+          {/* <Route path="/forum" element={<Forum/>} /> */}
         </Routes>
       </div>
     </BrowserRouter>
