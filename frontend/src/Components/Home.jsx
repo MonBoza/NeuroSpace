bb83780dc5f0cd2b9973cd5f936a66332f84c9c3
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-04-30 13:10:16 -0500

    Update Home component with Neurospace logo and about us section

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Neurospace from '../assets/img/Neurospace.png'
 
 const Home = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -15,9 +16,13 @@ const Home = () => {
 
   return (
     <>
+   
       <div className="min-h-screen flex flex-col justify-between items-center bg-gray-200 p-8 relative">
         <div className="mt-auto">
-          <h1 className="text-6xl font-bold text-center mb-8">
+        <div className="mt-8 left-0 right-0 p-4">
+        <img src={Neurospace} className=" mx-auto max-h-82 max-w-82 logo" />
+        </div>
+          <h1 className="text-6xl text-transform: uppercase font-bold text-center mb-8">
             Welcome to Neurospace {userName? userName : ""}
           </h1>
           <div className="bg-white rounded-lg shadow-md p-6 border border-gray-300 text-lg text-gray-800">
@@ -38,27 +43,34 @@ const Home = () => {
               with its own colors!
             </p>
             <button
-              className="bg-indigo-400 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded-full mt-6 block mx-auto"
+              className="bg-indigo-400 hover:bg-indigo-500 text-white font-bold py-2 mb-8 px-4 rounded-full mt-6 block mx-auto"
               onClick={handleRegister}
             >
               Register For an Account
             </button>
           </div>
         </div>
+        
         <div className="mb-8 px-4 mt-10 py-4">
-          <h1 className="text-6xl font-bold text-center mb-8">
-            About NeuroSpace
+          <h1 className="text-6xl text-transform: uppercase font-bold text-center mb-8">
+            About Us
           </h1>
           <div className="bg-white rounded-lg shadow-md p-6 border border-gray-300 text-lg text-gray-800">
             <p className="mb-4">
               Neurospace was born from the essence of Timothy, Ethan, and
-              Parker, embodying the very spirit of their being. These
-              remarkable individuals serve as shining examples, embodying
-              qualities we should all aspire to emulate.
+              Parker, embodying the very spirit of their being.
+              <p className="mb-4"> 
+              These remarkable individuals serve as shining examples, encompassing
+              qualities we should all aspire to emulate.</p>
             </p>
+           
           </div>
         </div>
       </div>
+      
+       
+       
+      
     </>
   );
 };
