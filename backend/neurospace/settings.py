b25792b4d26f36f7fc694b2e06d03c340b27960a
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-05-07 21:43:54 -0500

    Update build script and database configuration

--- backend/neurospace/settings.py
+++ backend/neurospace/settings.py
@@ -90,14 +90,14 @@ WSGI_APPLICATION = 'neurospace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:8000/Nuerospace',
+        default='postgresql://postgres:postgres@localhost:5432/neurospace',
         conn_max_age=600
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
